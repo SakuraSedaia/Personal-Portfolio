55ec88ca48d4e3240a9f66025a4f039cf4b392dc
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2020-07-12 01:30:53 -0500

    Reformatted Document using Beautify

--- responsive.js
+++ responsive.js
@@ -1,17 +1,16 @@
-
 // Dev Alert
 function openDevAlert() {
-  window.alert(
-    "This page is still under Development, please report any bugs to my email at endergfx6@gmail.com"
-  );
-  document.getElementById("devWarning").style.animationName =
-    "devWarningDeactivate";
-  document.getElementById("devWarning").style.animationIterationCount = "1";
-  document.getElementById("devWarning").style.animationDuration = ".3s";
-  document.getElementById("devWarning").style.cursor = "default";
-  document.getElementById("devWarning").removeAttribute("onclick");
+    window.alert(
+        "This page is still under Development, please report any bugs to my email at endergfx6@gmail.com"
+    );
+    document.getElementById("devWarning").style.animationName =
+        "devWarningDeactivate";
+    document.getElementById("devWarning").style.animationIterationCount = "1";
+    document.getElementById("devWarning").style.animationDuration = ".3s";
+    document.getElementById("devWarning").style.cursor = "default";
+    document.getElementById("devWarning").removeAttribute("onclick");
 }
 
 function openNavigation() {
-    
+
 }
