57e405ae8e0481a9bfe7ab3bae309c2891a18e46
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2020-07-12 01:59:45 -0500

    Added Navigation

--- responsive.js
+++ responsive.js
@@ -1,16 +1,24 @@
 // Dev Alert
 function openDevAlert() {
-    window.alert(
-        "This page is still under Development, please report any bugs to my email at endergfx6@gmail.com"
-    );
-    document.getElementById("devWarning").style.animationName =
-        "devWarningDeactivate";
-    document.getElementById("devWarning").style.animationIterationCount = "1";
-    document.getElementById("devWarning").style.animationDuration = ".3s";
-    document.getElementById("devWarning").style.cursor = "default";
-    document.getElementById("devWarning").removeAttribute("onclick");
+  window.alert(
+    "This page is still under Development, please report any bugs to my email at endergfx6@gmail.com"
+  );
+  document.getElementById("devWarning").style.animationName =
+    "devWarningDeactivate";
+  document.getElementById("devWarning").style.animationIterationCount = "1";
+  document.getElementById("devWarning").style.animationDuration = ".3s";
+  document.getElementById("devWarning").style.cursor = "default";
+  document.getElementById("devWarning").removeAttribute("onclick");
 }
-
+var isNavOpen;
 function openNavigation() {
-
+  if (isNavOpen != 0) {
+    document.getElementById("navWrapper").style.animationName = "navAnimateIn";
+    isNavOpen = 0;
+    document.getElementById("navigation").innerHTML = "menu_open";
+  } else if (isNavOpen != 1) {
+    document.getElementById("navWrapper").style.animationName = "navAnimateOut";
+    isNavOpen = 1;
+    document.getElementById("navigation").innerHTML = "menu";
+  }
 }
