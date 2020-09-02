2b9d751f68d677fd205ca574885aa93f6175205e
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2020-09-02 10:15:56 -0500

    Complete Rewrite and Integration of Bootstrap

--- assets/Scripts/datecheck.js
+++ assets/Scripts/datecheck.js
@@ -1,125 +1,27 @@
-var date = new Date();
-
-var dateMonth = date.getMonth();
-var dateDay = date.getDay();
-var dateYear = date.getYear();
-var timeHour = date.getHours();
-var dateDate = date.getDate();
-
-var christmasDate;
-var birthdayDate;
-
-var useDatecheckJS = true;
-
-var checkDate = setInterval("checkDateEvent()", 1000);
-
-function stopCheck() {
-  clearInterval(checkDate);
-}
-function checkDateEvent() {
-  console.info("Initiating Datecheck.js");
-
-  if (dateMonth == 11) {
-    if (dateDate == 20) {
-	  chistmasDate = true;
-	  console.info("Date is near Christmas");
-    } else if (dateDate == 21) {
-	  chistmasDate = true;
-	  console.info("Date is near Christmas");
-    } else if (dateDate == 22) {
-	  chistmasDate = true;
-	  console.info("Date is near Christmas");
-    } else if (dateDate == 23) {
-	  chistmasDate = true;
-	  console.info("Date is near Christmas");
-    } else if (dateDate == 24) {
-	  chistmasDate = true;
-	  console.info("Date is Christmas Eve (December 24th)");
-    } else if (dateDate == 25) {
-	  chistmasDate = true;
-	  console.info("Date is Christmas Day (December 25th)");
-    } else {
-      christmasDate = false;
-      console.error('Date is not Christmas');
-    }
-  } else if (dateMonth == 5) {
-    if (dateDate == 19) {
-	  birthdayDate = true;
-	  console.info("Today is Sakura's Birthday");
-    } else {
-      birthdayDate = false;
-      console.error("Today is not June 19 (Birthday)");
-    }
-  }
-
-  checkTime();
-} // End Special Event Checks
-
-var dateNight;
-
-function checkTime() {
-  if (timeHour >= 8) {
-    dateNight = true;
-  } else if (timeHour <= 6) {
-    dateNight = true;
-  } else {
-    dateNight = false;
-  }
-  setBackground();
-}
-
-var backgroundState = 0;
-
 /*
-0 = Default Background
-1 = Day Background
-2 = Night Background
-3 = Christmas Day Background
-4 = Christmas Night Background
-5 = Birthday Day Background
-6 = Birthday Night Background
+==========================
+NOTICE: Do not Distribute without Sakura's prior Concent
+==========================
+
+Datecheck.js is used to determine events and such
 */
 
-function setBackground() {
-  if (dateNight == false) {
-    if (christmasDate == false) {
-      if (birthdayDate == false) {
-        backgroundState = 1;
-      } else if (birthdayDate == true) {
-        backgroundState = 5;
-      }
-    } else if (christmasDate == true) {
-      backgroundState == 3;
-    }
-  } else if (dateNight == true) {
-    if (christmasDate == false) {
-      if (birthdayDate == false) {
-        backgroundState = 2;
-      } else if (birthdayDate == true) {
-        backgroundState = 6;
-      }
-    } else if (christmasDate == true) {
-      backgroundState == 4;
-    }
-  }
+setDate();
 
-  finalizeScript();
-}
+var fullJSDate, dateDay, dateMonth, dateYear, timeHour, timeUTCHour;
 
-function finalizeScript() {
-  if (backgroundState == 1) {
-    // Execute Functions
-  } else if (backgroundState == 2) {
-    // Execute Functions
-  } else if (backgroundState == 3) {
-    // Execute Functions
-  } else if (backgroundState == 4) {
-    // Execute Functions
-  } else if (backgroundState == 5) {
-    // Execute Functions
-  } else if (backgroundState == 6) {
-    // Execute Functions
-  } else if (backgroundState == 0) {
-    // Execute Functions
+function setDate() {
+  fullJSDate = new Date();
+
+  dateDay = fullJSDate.getDay; // Days Labeled 1-31
+  dateMonth = fullJSDate.getMonth; // Months labeled 0-11, January is 0 December is 11
+  dateYear = fullJSDate.getUTCFullYear; // Labaled as a full Year
+
+  timeHour = fullJSDate.getHours; // Hours displayed in 24hr clock
+
+  if(timeHour => 12) {
+    timeUTCHour = timeHour - 12 + "PM";
+  } else if(timeHour =< 12) {
+    timeUTCHour = timeHour + "AM";
   }
 }
