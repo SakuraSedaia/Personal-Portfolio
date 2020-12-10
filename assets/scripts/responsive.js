de02f49b1cffe2f35245368a8e663cb3a05e53a9
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2020-12-10 09:04:53 -0600

    Update responsive.js

--- assets/scripts/responsive.js
+++ assets/scripts/responsive.js
@@ -110,3 +110,11 @@ function downloadSLACME (state) {
     console.error('Rig download Disabled')
   }
 }
+
+// Import Dev Tool Script
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = "dev-mode.js";  // set its src to the provided URL
+
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
