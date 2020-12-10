04a7153ef93b9557072c93bd2743a9a9cae76a62
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2020-12-10 09:01:24 -0600

    Update responsive.js

--- assets/scripts/responsive.js
+++ assets/scripts/responsive.js
@@ -110,17 +110,3 @@ function downloadSLACME (state) {
     console.error('Rig download Disabled')
   }
 }
-
-// Developer Tools
-var devMode = false
-var useDevRefresh = true
-var useDevWarning = true
-var devRefreshInterval = 3000
-
-if (devMode == true && useDevRefresh == true) {
-  var devConsoleWarning = console.warn('Developer Tools Activated')
-  window.setTimeout('window.location.reload()', devRefreshInterval)
-	
-} else if (devMode == true && useDevWarning == true) { // Checks if Dev Warning is being used and then injects the Code
-	document.getElementById('devWarning').innerHTML = '<strong>Warning: </strong>This site is still in Development, please report any issues to Sakura at <a href="#" onclick="emailSaki()">this Email</a>'
-}
