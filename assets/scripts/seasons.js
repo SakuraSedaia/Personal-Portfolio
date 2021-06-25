bcaf4c5c181671ee9e58717e83e851911bd41f4d
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2021-06-24 23:27:08 -0500

    Adjustment to Backend Files

--- assets/scripts/seasons.js
+++ assets/scripts/seasons.js
@@ -1,23 +1,14 @@
 var header = document.getElementById('header')
 var xmasCounter = document.getElementById('xmasCounter')
-
-// Global Variables
 var xmasDate = new Date()
-
 var xmasMonth = xmasDate.getMonth()
 var xmasDay = xmasDate.getDate()
-
 if ((xmasMonth == 11 && xmasDay <= 25) || (xmasMonth == 10 && xmasDay > 26)) {
   christmas = true
 } else {
   christmas = false
 }
-
 if (christmas == true) {
-  console.info(
-    'DEBUG: It is Christmas Time, changing to Christmas Header Image'
-  )
-
   // Change Backgrounds
   header.style.backgroundImage =
     'url(assets/images/WPRenders/SakiAzzy-Wallpaper-Fixed-Composited.png)'
@@ -31,15 +22,10 @@ if (christmas == true) {
   }
 }
 if (christmas == false) {
-  console.info(
-    'DEBUG: It is not Christmas Time, changing back to Normal Header Image'
-  )
-
   // Change Backgrounds
   header.style.backgroundImage =
     'url(assets/images/WPRenders/Happy_Bday.jpg)'
   header.style.backgroundPosition = 'center'
-
   // Modify HTML Elements
   xmasCounter.innerText = 'Not Christmas Time'
   document.getElementById('xmasContainer').style.visibility = 'hidden'
