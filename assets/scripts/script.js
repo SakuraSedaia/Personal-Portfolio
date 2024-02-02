9aeb8de5de59986b7eb3e8226aa48cf7b66244a8
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2024-02-02 01:49:01 -0600

    Updates people, Updates!
    Revised Rig Download Page
    
    Removed Works and Lost Angel sections on Home Page
    
    Changed "Home" to
    "About Me" in Nav elements

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,6 +13,31 @@ function openExternal(url, trusted) {
     }
 }
 
+function downloadSACR(file, dev) {
+    if(file==null) {
+        window.alert("This download does not exist")
+    } else if (dev != 1) {
+        var filepath = window.location.href.substring(0, (window.location.href.length - 9)) + "assets/rig-storage/sacr-directory/" + file;
+
+        console.log("Initiated Download request for file " + file + ", from " + filepath)
+        if (window.confirm(`You are about to download a file from the site, are you sure you want to continue?\n\nSite Link: ` + filepath)) {
+            var downloadWin = window.open(filepath, "File Download", "width = 100px;");
+            if (downloadWin.getElementsByTagName("h1").innerText = "File not found") {
+                console.log("File not Found");
+            }
+        }
+    } else if (dev == 1) {
+        var filepath = window.location.href.substring(0, (window.location.href.length - 9)) + "assets/rig-storage/sacr-experimental/" + file;
+
+        console.log("Initiated Download request for file " + file + ", from " + filepath)
+        if (window.confirm(`You are about to download a file from the site, are you sure you want to continue?\n\nSite Link: ` + filepath)) {
+            var downloadWin = window.open(filepath, "File Download", "width = 100px;");
+            if (downloadWin.getElementsByTagName("h1").innerText = "File not found") {
+                console.log("File not Found");
+            }
+        }
+    }
+}
 function downloadRig(file) {
     if(file==null) {
         window.alert("This download does not exist")
@@ -28,3 +53,4 @@ function downloadRig(file) {
         }
     }
 }
+
