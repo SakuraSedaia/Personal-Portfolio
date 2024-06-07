1e5b2458f2de80513189ea6f2f6a3c83e1edd449
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2024-06-06 19:01:47 -0500

    Finalized SACR E7 Update

---
+++ assets/docs/SACR E7 Changelog.md
@@ -0,0 +1,21 @@
+
+# Sakura's Advanced Character Rig
+Latest Version: R7_Dev_01
+--------
+SACR R7 Dev Build 01:
+### Current Changes:
+- Optimized Materials for Blender 4.0
+- Optimized Drivers
+- Prepared Rig for Updates
+
+SACR R7 Dev Build 02:
+### Current Changes:
+- Removed Armor Completely
+- Deleted Redundant Drivers
+- Improved Molar controls
+- Reset all modified Vectors on Mesh
+- Added Auto-Smooth Modifiers to:
+    Face
+    Torso
+    Left Arm
+    Right Arm
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -28,7 +28,6 @@ function downloadSACR(file, dev) {
         }
     } else if (dev == 1) {
         var filepath = window.location.href.substring(0, (window.location.href.length - 9)) + "assets/rig-storage/sacr-experimental/" + file;
-
         console.log("Initiated Download request for file " + file + ", from " + filepath)
         if (window.confirm(`You are about to download a file from the site, are you sure you want to continue?\n\nSite Link: ` + filepath)) {
             var downloadWin = window.open(filepath, "File Download", "width = 100px;");
@@ -44,7 +43,6 @@ function downloadRig(file) {
         window.alert("This download does not exist")
     } else {
         var filepath = window.location.href.substring(0, (window.location.href.length - 9)) + "assets/rig-storage/" + file;
-
         console.log("Initiated Download request for file " + file + ", from " + filepath)
         if (window.confirm(`You are about to download a file from the site, are you sure you want to continue?\n\nSite Link: ` + filepath)) {
             var downloadWin = window.open(filepath, "File Download", "width = 100px;");
@@ -59,7 +57,13 @@ function copyEmail(text) {
     window.alert('Email "' + text + '" copied to clipboard');
     navigator.clipboard.writeText(text);
 }
+
 function copyDiscUser(text) {
     window.alert('Discord Name "' + text + '" copied to clipboard');
     navigator.clipboard.writeText(text);
+}
+
+let mdPath = "assets/docs/"
+function openMarkdown(md) {
+    window.open(mdPath + md, "_blank")
 }
