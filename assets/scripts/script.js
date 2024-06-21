f172c2d0f95619c97a381e6b63e810b5ead13f32
SakuraSedaia <61118816+SakuraSedaia@users.noreply.github.com>
2024-06-21 03:12:25 -0500

    Updated rig.html
    Added Assets Section for Asset Rigs
    Added Links to both SACR Githubs
    
    Added Github to all pages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+
+
 // Link Handling
 function openInternal(url) {
     window.open(url, "_self");
@@ -13,6 +15,20 @@ function openExternal(url, trusted) {
     }
 }
 
+
+
+function copyEmail(text) {
+    window.alert('Email "' + text + '" copied to clipboard');
+    navigator.clipboard.writeText(text);
+}
+
+function copyDiscUser(text) {
+    window.alert('Discord Name "' + text + '" copied to clipboard');
+    navigator.clipboard.writeText(text);
+}
+
+
+// Rigs.html
 function downloadSACR(file, dev) {
     if (file == null) {
         window.alert("This download does not exist")
@@ -28,6 +44,7 @@ function downloadSACR(file, dev) {
         }
     } else if (dev == 1) {
         var filepath = window.location.href.substring(0, (window.location.href.length - 9)) + "assets/rig-storage/sacr-experimental/" + file;
+
         console.log("Initiated Download request for file " + file + ", from " + filepath)
         if (window.confirm(`You are about to download a file from the site, are you sure you want to continue?\n\nSite Link: ` + filepath)) {
             var downloadWin = window.open(filepath, "File Download", "width = 100px;");
@@ -37,33 +54,42 @@ function downloadSACR(file, dev) {
         }
     }
 }
-
-function downloadRig(file) {
+var rootDir = "assets/rig-storage/asset-rigs/"
+var rigDir = [
+    rootDir + "character-addons/",
+    rootDir + "models/"
+]
+function downloadRig(file,cat) {
     if (file == null) {
         window.alert("This download does not exist")
     } else {
-        var filepath = window.location.href.substring(0, (window.location.href.length - 9)) + "assets/rig-storage/" + file;
+        var filepath = window.location.href.substring(0, (window.location.href.length - 9)) + rigDir[cat] + file;
+
         console.log("Initiated Download request for file " + file + ", from " + filepath)
         if (window.confirm(`You are about to download a file from the site, are you sure you want to continue?\n\nSite Link: ` + filepath)) {
             var downloadWin = window.open(filepath, "File Download", "width = 100px;");
-            if (downloadWin.getElementsByTagName("h1").innerText = "File not found") {
-                console.log("File not Found");
-            }
         }
     }
-}
-
-function copyEmail(text) {
-    window.alert('Email "' + text + '" copied to clipboard');
-    navigator.clipboard.writeText(text);
-}
-
-function copyDiscUser(text) {
-    window.alert('Discord Name "' + text + '" copied to clipboard');
-    navigator.clipboard.writeText(text);
 }
 
 let mdPath = "assets/docs/"
 function openMarkdown(md) {
-    window.open(mdPath + md, "_blank")
+    if (md == null) {
+        window.alert("File does not exist")
+    } else {
+        window.open(mdPath + md, "_blank")
+    }
+}
+
+var catList = document.getElementsByClassName('catagory')
+var ico;
+function expandCatagory(index) {
+    ico = catList[index].childNodes[1].childNodes[3]
+    if (ico.getAttribute('active') == "false") {
+        ico.setAttribute('active', true)
+        catList[index].setAttribute('expanded', true);
+    } else {
+        ico.setAttribute('active', false)
+        catList[index].setAttribute('expanded', false);
+    }
 }
